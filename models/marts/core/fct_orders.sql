5a3272f0360e53dfc6f521ad9a4478c3ed17ec5a
germandataengineering@gmail.com <germandataengineering@gmail.com>
2023-11-22 18:09:39 +0000

    tabla de hechos de orders completa y funcionando

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -9,14 +9,14 @@ WITH stg_orders AS (
     FROM {{ ref('stg_orders') }}
 ),
 
-WITH stg_order_items AS (
+stg_order_items AS (
     SELECT *
-    FROM FROM {{ ref('stg_order_items') }}
-)
+    FROM {{ ref('stg_order_items') }}
+),
 
 renamed_casted AS (
     SELECT
-          order_id 
+          stg_orders.order_id 
         , user_id 
         , promo_id
         , address_id
@@ -31,9 +31,11 @@ renamed_casted AS (
         , delivered_at_utc
 		, DATEDIFF(day, created_at_utc, delivered_at_utc) AS days_to_deliver
         , status_order
-        , date_load
+        , stg_orders.date_load
     FROM stg_orders
     JOIN stg_order_items ON stg_order_items.order_id = stg_orders.order_id
+    GROUP BY stg_orders.order_id, user_id , promo_id, address_id, created_at_utc, item_order_cost_usd, shipping_cost_usd, total_order_cost_usd, tracking_id,
+     shipping_service, estimated_delivery_at_utc, delivered_at_utc, status_order, stg_orders.date_load
     )
 
 SELECT * FROM renamed_casted
