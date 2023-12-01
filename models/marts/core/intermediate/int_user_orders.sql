63a6aee895e114f4a3b24cc0a4094080292b9564
germandataengineering@gmail.com <germandataengineering@gmail.com>
2023-12-01 17:49:57 +0000

    casi terminado caso de uso de marketing

--- models/marts/core/intermediate/int_user_orders.sql
+++ models/marts/core/intermediate/int_user_orders.sql
@@ -19,17 +19,28 @@ stg_addresses AS (
     FROM {{ ref('stg_addresses') }}
 ),
 
+stg_promos AS (
+    SELECT *
+    FROM {{ ref('stg_promos') }}
+),
+
+
 renamed_casted AS (
     SELECT 
           stg_orders.user_id
         , stg_users.first_name
         , stg_users.last_name
         , stg_users.phone_number
+        , stg_users.email
         , COUNT(stg_orders.order_id) as total_orders_by_user
         , CAST(SUM(total_order_cost_usd) AS DECIMAL(18, 2)) as user_total_expenditure_usd
+        , CAST(SUM(shipping_cost_usd) AS DECIMAL(18, 2)) as user_total_shipping_cost_usd
+        , CAST(SUM(stg_promos.discount_usd) AS DECIMAL(18, 2)) as user_total_discount_usd
+        --, productos distintos comprados 
     FROM stg_orders
+    JOIN stg_promos ON stg_orders.promo_id = stg_promos.promo_id
     JOIN stg_users ON stg_orders.user_id = stg_users.user_id
-    GROUP BY stg_orders.user_id, stg_users.first_name, stg_users.last_name, stg_users.phone_number
+    GROUP BY stg_orders.user_id, stg_users.first_name, stg_users.last_name, stg_users.phone_number, stg_users.email
     ORDER BY user_total_expenditure_usd DESC
 )
 
