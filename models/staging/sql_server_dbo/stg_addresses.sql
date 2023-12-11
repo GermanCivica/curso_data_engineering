8feadb44c1b1826cf1b52d2ed5ab713ee983a69f
germandataengineering@gmail.com <germandataengineering@gmail.com>
2023-12-11 14:08:35 +0000

    full functional ingestion system

--- models/staging/sql_server_dbo/stg_addresses.sql
+++ models/staging/sql_server_dbo/stg_addresses.sql
@@ -1,14 +1,21 @@
 
 {{
-  config(
-    materialized='view'
-  )
+    config(
+        materialized='incremental',
+        unique_key='address_id',
+        on_schema_change='fail'
+    )
 }}
 
 WITH src_addresses_orders AS (
     SELECT * 
     FROM {{ source('sql_server_dbo', 'addresses') }}
-    ),
+{% if is_incremental() %}
+
+	  where _fivetran_synced > (select max(date_load_utc) from {{ this }})
+
+{% endif %}
+),
 
 renamed_casted AS (
     SELECT
